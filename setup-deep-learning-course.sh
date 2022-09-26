ccf8bfd27d693c343c08827c48c51938ecbb0d07
Esteban Osorio <eosorio@nlhpc.cl>
2022-09-26 15:15:13 -0300

    Adding verbose information

--- setup-deep-learning-course.sh
+++ setup-deep-learning-course.sh
@@ -1,12 +1,19 @@
+echo "Downloading README and requirement files from GIT"
+wget https://github.com/nlhpc-training/Deep-Learning-Course.git
+wget https://github.com/nlhpc-training/Deep-Learning-Course.git
+
+echo "Loading modules and creating Python virtual environment"
 ml purge
 ml intel
 ml Python
 python -mvenv curso_dl
+
+echo "Loading Python requirements"
 source .curso_dl/bin/activate
 pip install -r requirements.txt
 pip install yolo5
 
-
+echo "Checking and editing .bashrc file"
 if grep -q "ml Python" ~/.bashrc; then
     echo "Python already loaded at .bashrc file"
 else
@@ -18,3 +25,5 @@ if grep -q "source .curso_dl/bin/activate" ~/.bashrc; then
 else
     echo "source .curso_dl/bin/activate" >> ~/.bashrc
 fi
+
+echo "Done!"
